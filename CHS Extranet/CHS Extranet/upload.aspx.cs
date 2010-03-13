de67869e3c9523b8ee279c9886fc17b51d31c931
nickbro_cp <SND\nickbro_cp>
2010-03-13 13:50:04 +0000

    Upload Filters

--- CHS Extranet/CHS Extranet/upload.aspx.cs
+++ CHS Extranet/CHS Extranet/upload.aspx.cs
@@ -6,14 +6,66 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.DirectoryServices.AccountManagement;
 using System.Configuration;
+using CHS_Extranet.Configuration;
 
 namespace CHS_Extranet
 {
     public partial class upload : System.Web.UI.Page
     {
+        private String _DomainDN;
+        private String _ActiveDirectoryConnectionString;
+        private PrincipalContext pcontext;
+        private UserPrincipal up;
+        private extranetConfig config;
+
+        private bool isAuth(uploadfilter filter)
+        {
+            if (filter.EnableFor == "All") return true;
+            else if (filter.EnableFor != "None")
+            {
+                bool vis = false;
+                foreach (string s in filter.EnableFor.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    GroupPrincipal gp = GroupPrincipal.FindByIdentity(pcontext, s);
+                    if (!vis) vis = up.IsMemberOf(gp);
+                }
+                return vis;
+            }
+            return false;
+        }
+
+        public string Username
+        {
+            get
+            {
+                if (User.Identity.Name.Contains('\\'))
+                    return User.Identity.Name.Remove(0, User.Identity.Name.IndexOf('\\') + 1);
+                else return User.Identity.Name;
+            }
+        }
+
+        protected override void OnInitComplete(EventArgs e)
+        {
+            config = ConfigurationManager.GetSection("extranetConfig") as extranetConfig;
+            ConnectionStringSettings connObj = ConfigurationManager.ConnectionStrings[config.ADSettings.ADConnectionString];
+            if (connObj != null) _ActiveDirectoryConnectionString = connObj.ConnectionString;
+            if (string.IsNullOrEmpty(_ActiveDirectoryConnectionString))
+                throw new Exception("The connection name 'activeDirectoryConnectionString' was not found in the applications configuration or the connection string is empty.");
+            if (_ActiveDirectoryConnectionString.StartsWith("LDAP://"))
+                _DomainDN = _ActiveDirectoryConnectionString.Remove(0, _ActiveDirectoryConnectionString.IndexOf("DC="));
+            else throw new Exception("The connection string specified in 'activeDirectoryConnectionString' does not appear to be a valid LDAP connection string.");
+            pcontext = new PrincipalContext(ContextType.Domain, null, _DomainDN, config.ADSettings.ADUsername, config.ADSettings.ADPassword);
+            up = UserPrincipal.FindByIdentity(pcontext, IdentityType.SamAccountName, Username);
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            Xaml1.InitParameters = "UploadPage=FileUpload.ashx/" + Request.QueryString["path"] + "/,UploadChunkSize=131072";
+            List<string> filters = new List<string>();
+            foreach (uploadfilter f in config.UploadFilters)
+                if (isAuth(f)) filters.Add(f.ToString());
+            string fs = string.Join("|", filters.ToArray());
+            if (!string.IsNullOrEmpty(fs)) fs = ",Filter=" + fs;
+            Xaml1.InitParameters = string.Format("UploadPage=FileUpload.ashx/{0}/,UploadChunkSize=131072{1}", Request.QueryString["path"], fs);
         }
     }
 }
