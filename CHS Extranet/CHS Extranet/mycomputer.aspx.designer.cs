81144095f1e204069354086a61a155125219a2fa
nickbro_cp <SND\nickbro_cp>
2010-03-14 15:24:59 +0000

    Added Breadcrumbs to MyComputer

--- CHS Extranet/CHS Extranet/mycomputer.aspx.designer.cs
+++ CHS Extranet/CHS Extranet/mycomputer.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace CHS_Extranet {
         protected global::System.Web.UI.WebControls.HyperLink fileuploadlink;
         
         /// <summary>
+        /// breadcrumbrepeater control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater breadcrumbrepeater;
+        
+        /// <summary>
         /// browserrepeater control.
         /// </summary>
         /// <remarks>
