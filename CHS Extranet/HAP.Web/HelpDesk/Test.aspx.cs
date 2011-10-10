8bcf29c05124845a3610a6beffd7d78f560a0415
nickbro_cp <SND\nickbro_cp>
2011-10-10 15:43:52 +0000

    New Help Desk, plus new booking system fix v7.4.1010.1640

--- CHS Extranet/HAP.Web/HelpDesk/Test.aspx.cs
+++ CHS Extranet/HAP.Web/HelpDesk/Test.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using HAP.AD;
 
 namespace HAP.Web.HelpDesk
 {
@@ -11,7 +12,15 @@ namespace HAP.Web.HelpDesk
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            adminbookingpanel.Visible = adminupdatepanel.Visible = User.IsInRole("Domain Admins");
+            if (User.IsInRole("Domain Admins"))
+            {
+                userlist.Items.Clear();
+                foreach (UserInfo user in ADUtils.FindUsers())
+                    if (user.DisplayName == user.UserName) userlist.Items.Add(new ListItem(user.UserName, user.UserName.ToLower()));
+                    else userlist.Items.Add(new ListItem(string.Format("{0} - ({1})", user.UserName, user.DisplayName), user.UserName.ToLower()));
+                userlist.SelectedValue = ADUser.UserName.ToLower();
+            }
         }
     }
 }
