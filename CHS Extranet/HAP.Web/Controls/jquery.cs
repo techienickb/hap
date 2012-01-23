6cf37706d3c78a5d8c72a9c8d4392559c6ee13d0
nickbro_cp <nick@nbdev.co.uk>
2012-01-23 12:14:58 +0000

    Added jquery mobile

--- CHS Extranet/HAP.Web/Controls/jquery.cs
+++ CHS Extranet/HAP.Web/Controls/jquery.cs
@@ -7,26 +7,25 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace HAP.Web.Controls
 {
     [ToolboxData("<{0}:jQuery runat=\"server\" />")]
-    public class jQuery : WebControl
+    public class jQuery : Control
     {
-        public override void RenderBeginTag(HtmlTextWriter writer)
-        {
-        }
-
-        protected override void RenderContents(HtmlTextWriter output)
+        protected override void RenderControl(HtmlTextWriter output)
         {
             output.WriteLine("<script src=\"" + ResolveUrl("~/Scripts/jquery-1.7.1.min.js") + "\" type=\"text/javascript\"></script>");
             output.WriteLine("<script src=\"" + ResolveUrl("~/Scripts/jquery-ui-1.8.16.custom.min.js") + "\" type=\"text/javascript\"></script>");
+            Regex regex = new Regex("(XBLWP7|ZuneWP|iPhone|iPad|Android|BlackBerry)", RegexOptions.IgnoreCase);
+            if (regex.IsMatch(HttpContext.Current.Request.UserAgent))
+            {
+                output.WriteLine("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />");
+                output.WriteLine("<link href=\"" + ResolveUrl("~/style/jquery.mobile-1.0.css") + "\"  rel=\"stylesheet\" type=\"text/css\" />");
+                output.WriteLine("<script src=\"" + ResolveUrl("~/Scripts/jquery.mobile-1.0.min.js") + "\" type=\"text/javascript\"></script>");
+            }
             output.WriteLine("<script src=\"" + ResolveUrl("~/api/js") + "\" type=\"text/javascript\"></script>");
-            
-        }
-
-        public override void RenderEndTag(HtmlTextWriter writer)
-        {
         }
     }
 }
---
+++ CHS Extranet/HAP.Web/Scripts/hap.web.js.mobile.js
@@ -0,0 +1,7 @@
+﻿/*HAP.Web.JS.Mobile.js - Copyright © 2012 nb development - Version 1 */
+hap.mobile = {
+    Init: function () {
+        $.mobile.fixedToolbars.setTouchToggleEnabled(true);
+        $.mobile.fixedToolbars.show(true);
+    }
+};
