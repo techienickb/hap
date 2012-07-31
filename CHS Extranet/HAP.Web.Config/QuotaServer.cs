5f999d77b20303c02f55913bfd2e9acfbca8748d
nickbro_cp <SND\nickbro_cp>
2012-07-31 21:04:50 +0000

    changes to the quota service

--- CHS Extranet/HAP.Web.Config/QuotaServer.cs
+++ CHS Extranet/HAP.Web.Config/QuotaServer.cs
@@ -16,10 +16,12 @@ namespace HAP.Web.Configuration
             Server = node.Attributes["server"].Value;
             Expression = HttpContext.Current.Server.HtmlDecode(node.InnerText);
             Drive = node.Attributes["drive"].Value.ToCharArray()[0];
+            FSRM = node.Attributes["fsrm"] == null ? false : bool.Parse(node.Attributes["fsrm"].Value);
         }
 
         public string Expression { get; set; }
         public string Server { get; set; }
         public char Drive { get; set; }
+        public bool FSRM { get; set; }
     }
 }
