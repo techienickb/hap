0d97ed9a1751eb44136c49b1f5411b5ef81f4223
nickbro_cp <SND\nickbro_cp>
2012-08-02 12:27:20 +0000

    Added: CSS to Remove the Padding to the Body/HTML Fixed: Where the Me items would appear in the management tab

--- CHS Extranet/HAP.Web/Properties/AssemblyInfo.cs
+++ CHS Extranet/HAP.Web/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("8.0.0802.1200")]
-[assembly: AssemblyFileVersion("8.0.0802.1200")]
+[assembly: AssemblyVersion("8.0.0802.1400")]
+[assembly: AssemblyFileVersion("8.0.0802.1400")]
