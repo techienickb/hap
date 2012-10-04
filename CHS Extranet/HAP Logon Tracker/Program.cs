b0b83ff7d668703edee6c917a210f19707412edf
nickbro_cp <nick@nbdev.co.uk>
2012-10-04 11:27:26 +0000

    Added: Silent check for the logon tracker

--- CHS Extranet/HAP Logon Tracker/Program.cs
+++ CHS Extranet/HAP Logon Tracker/Program.cs
@@ -14,9 +14,12 @@ namespace HAP.Logon.Tracker
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            if (args.Length == 0) Application.Run(new Loading(Action.Logon, "https://folders.crickhowell-hs.powys.sch.uk/hap/"));
-            else if (args[0].StartsWith("http")) Application.Run(new Loading(Action.Clear, args[0]));
-            else Application.Run(new Loading(Action.Logon, args[1]));
+            bool silent = false;
+            foreach (string s in args)
+                if (s.ToLower().EndsWith("silent")) { silent = true; break; }
+            if (args.Length == 0) Application.Run(new Loading(Action.Logon, "https://folders.crickhowell-hs.powys.sch.uk/hap/", silent));
+            else if (args[0].StartsWith("http")) Application.Run(new Loading(Action.Clear, args[0], silent));
+            else Application.Run(new Loading(Action.Logon, args[1], silent));
         }
     }
 }
