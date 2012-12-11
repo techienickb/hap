f12f22eba2ecfccf46c58df2226a692f3ce360e9
nickbro_cp <nick@nbdev.co.uk>
2012-12-11 13:05:29 +0000

    Fixed: Switched to using the outlook/exchange thumbnailPhoto instead of jpegPhoto

--- CHS Extranet/HAP.Web/API/MyPicHandler.cs
+++ CHS Extranet/HAP.Web/API/MyPicHandler.cs
@@ -49,7 +49,7 @@ namespace HAP.Web.API
 
                         using (DirectoryEntry user = new DirectoryEntry(result.Path))
                         {
-                            byte[] data = user.Properties["jpegPhoto"].Value as byte[];
+                            byte[] data = user.Properties["thumbnailPhoto"].Value as byte[];
 
                             if (data != null)
                             {
