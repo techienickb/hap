a9e8da4f25b01b4de0deb84e2390b961b36a8dd3
nickbro_cp <SND\nickbro_cp>
2012-12-13 15:14:06 +0000

    Added: Resize commands to the My Pic handler Added: Center CSS to the User Drop Down Icon handler

--- CHS Extranet/HAP.Web/API/MyPicHandler.cs
+++ CHS Extranet/HAP.Web/API/MyPicHandler.cs
@@ -18,6 +18,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using HAP.Data.ComputerBrowser;
 using System.DirectoryServices;
+using System.Drawing.Drawing2D;
 
 namespace HAP.Web.API
 {
@@ -39,37 +40,101 @@ namespace HAP.Web.API
             {
                 HAP.AD.User _user = new HAP.AD.User();
                 _user.Authenticate(hapConfig.Current.AD.User, hapConfig.Current.AD.Password);
+                string errorlist = "";
                 try
                 {
                     _user.ImpersonateContained();
                     using (DirectorySearcher dsSearcher = new DirectorySearcher())
                     {
+                        errorlist += "Creating Directory Search and Searching for then current user\n";
                         dsSearcher.Filter = "(&(objectClass=user) (cn=" + ((HAP.AD.User)Membership.GetUser()).UserName + "))";
                         dsSearcher.PropertiesToLoad.Add("thumbnailPhoto");
-                        SearchResult result = dsSearcher.FindOne();
+                        SearchResultCollection results = dsSearcher.FindAll();
 
-                        using (DirectoryEntry user = new DirectoryEntry(result.Path))
+                        errorlist += "Found " + results.Count + " results, processing 1st result\n";
+                        if (results.Count > 0)
                         {
-                            byte[] data = user.Properties["thumbnailPhoto"].Value as byte[];
-
-                            if (data != null)
+                            errorlist += "Found " + results[0].Properties["thumbnailPhoto"].Count + " thumnbnailPhotos\n";
+                            if (results[0].Properties["thumbnailPhoto"].Count > 0)
                             {
-                                using (MemoryStream s = new MemoryStream(data))
+                                byte[] data = results[0].Properties["thumbnailPhoto"][0] as byte[];
+                                if (data != null)
                                 {
-                                    context.Response.ContentType = "image/png";
-                                    MemoryStream m = new MemoryStream();
-                                    Bitmap.FromStream(s).Save(m, ImageFormat.Png);
-                                    m.WriteTo(context.Response.OutputStream);
+                                    errorlist += "Data found, making picture\n";
+                                    using (MemoryStream s = new MemoryStream(data))
+                                    {
+                                        context.Response.ContentType = "image/png";
+                                        MemoryStream m = new MemoryStream();
+                                        Image i = Bitmap.FromStream(s);
+                                        FixedSize(i, 92, 92).Save(m, ImageFormat.Png);
+                                        m.WriteTo(context.Response.OutputStream);
+                                    }
                                 }
+                                else context.Response.Redirect("~/api/tiles/icons/128/128/images/icons/metro/folders-os/UserNo-Frame.png");
                             }
                             else context.Response.Redirect("~/api/tiles/icons/128/128/images/icons/metro/folders-os/UserNo-Frame.png");
                         }
+                        else context.Response.Redirect("~/api/tiles/icons/128/128/images/icons/metro/folders-os/UserNo-Frame.png");
                     }
+                    //throw new Exception();
                 }
-                catch { context.Response.Redirect("~/api/tiles/icons/128/128/images/icons/metro/folders-os/UserNo-Frame.png"); }
-                finally { _user.EndContainedImpersonate(); }
+                catch (Exception e)
+                {
+                    throw new Exception(errorlist, e);
+                    //context.Response.Redirect("~/api/tiles/icons/128/128/images/icons/metro/folders-os/UserNo-Frame.png");
+                }
+                finally {  }
             }
             else context.Response.Redirect("~/api/tiles/icons/128/128/images/icons/metro/folders-os/UserNo-Frame.png");
+        }
+
+        private Image FixedSize(Image imgPhoto, int Width, int Height)
+        {
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+            int sourceX = 0;
+            int sourceY = 0;
+            int destX = 0;
+            int destY = 0;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)Width / (float)sourceWidth);
+            nPercentH = ((float)Height / (float)sourceHeight);
+            if (nPercentH < nPercentW)
+            {
+                nPercent = nPercentH;
+                destX = System.Convert.ToInt16((Width -
+                              (sourceWidth * nPercent)) / 2);
+            }
+            else
+            {
+                nPercent = nPercentW;
+                destY = System.Convert.ToInt16((Height -
+                              (sourceHeight * nPercent)) / 2);
+            }
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap bmPhoto = new Bitmap(Width, Height,
+                              PixelFormat.Format32bppArgb);
+            bmPhoto.SetResolution(imgPhoto.HorizontalResolution,
+                             imgPhoto.VerticalResolution);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+            grPhoto.Clear(Color.Transparent);
+            grPhoto.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            grPhoto.DrawImage(imgPhoto,
+                new Rectangle(destX, destY, destWidth, destHeight),
+                new Rectangle(sourceX, sourceY, sourceWidth, sourceHeight),
+                GraphicsUnit.Pixel);
+
+            grPhoto.Dispose();
+            return bmPhoto;
         }
 
     }
