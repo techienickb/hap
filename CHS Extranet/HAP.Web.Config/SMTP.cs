ce2d82d73f4f62440dabf10f03c56e2f55f1cbc5
nickbro_cp <nick@nbdev.co.uk>
2013-02-13 13:33:03 +0000

    Added: Encryption for the passwords of the SMTP Added: Upgrade script for v9 release Set: Assembly Info to v9

--- CHS Extranet/HAP.Web.Config/SMTP.cs
+++ CHS Extranet/HAP.Web.Config/SMTP.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Web;
 using System.Xml;
 
 namespace HAP.Web.Configuration
@@ -54,8 +57,66 @@ namespace HAP.Web.Configuration
         }
         public string Password
         {
-            get { return el.GetAttribute("password"); }
-            set { el.SetAttribute("password", value); }
+            get
+            {
+                if (string.IsNullOrEmpty(el.GetAttribute("password"))) return "";
+                RijndaelManaged aesAlg = null;
+                string plaintext = null;
+
+                try
+                {
+                    Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(_key, _salt);
+
+                    aesAlg = new RijndaelManaged();
+                    aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+                    aesAlg.IV = key.GetBytes(aesAlg.BlockSize / 8);
+                    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                    byte[] bytes = Convert.FromBase64String(el.GetAttribute("password"));
+                    using (MemoryStream msDecrypt = new MemoryStream(bytes))
+                    {
+                        using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (StreamReader srDecrypt = new StreamReader(csDecrypt)) plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+                finally
+                {
+                    if (aesAlg != null) aesAlg.Clear();
+                }
+
+                return plaintext;
+            }
+            set
+            {
+                string outStr = "";
+                if (string.IsNullOrEmpty(value)) el.SetAttribute("password", "");
+                RijndaelManaged aesAlg = null;
+                try
+                {
+                    Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(_key, _salt);
+                    aesAlg = new RijndaelManaged();
+                    aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+                    aesAlg.IV = key.GetBytes(aesAlg.BlockSize / 8);
+                    ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                    using (MemoryStream msEncrypt = new MemoryStream())
+                    {
+                        using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                            {
+                                swEncrypt.Write(value);
+                            }
+                        }
+                        outStr = Convert.ToBase64String(msEncrypt.ToArray());
+                    }
+                }
+                finally
+                {
+                    if (aesAlg != null) aesAlg.Clear();
+                }
+                el.SetAttribute("password", outStr);
+            }
         }
         public string FromUser
         {
@@ -72,6 +133,9 @@ namespace HAP.Web.Configuration
             get { if (el.GetAttribute("exchange") == null) return ""; return el.GetAttribute("exchange"); }
             set { el.SetAttribute("exchange", value); }
         }
+        static byte[] _salt = Encoding.ASCII.GetBytes("zsKrYE328e");
+        static byte[] _salt2 = Encoding.ASCII.GetBytes("ys8cQPOvF7");
+        static string _key = "0En9wSkynoVZBdAxswdg" + HttpContext.Current.Server.MachineName + "3iob91Rwzz8IzTvfBreW";
         public string ImpersonationUser
         {
             get { return el.GetAttribute("impersonationuser"); }
@@ -79,8 +143,66 @@ namespace HAP.Web.Configuration
         }
         public string ImpersonationPassword
         {
-            get { return el.GetAttribute("impersonationpassword"); }
-            set { el.SetAttribute("impersonationpassword", value); }
+            get 
+            {
+                if (string.IsNullOrEmpty(el.GetAttribute("impersonationpassword"))) return "";
+                RijndaelManaged aesAlg = null;
+                string plaintext = null;
+
+                try
+                {
+                    Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(_key, _salt2);
+
+                    aesAlg = new RijndaelManaged();
+                    aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+                    aesAlg.IV = key.GetBytes(aesAlg.BlockSize / 8);
+                    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                    byte[] bytes = Convert.FromBase64String(el.GetAttribute("impersonationpassword"));
+                    using (MemoryStream msDecrypt = new MemoryStream(bytes))
+                    {
+                        using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (StreamReader srDecrypt = new StreamReader(csDecrypt)) plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+                finally
+                {
+                    if (aesAlg != null) aesAlg.Clear();
+                }
+
+                return plaintext;
+            }
+            set
+            {
+                string outStr = "";
+                if (string.IsNullOrEmpty(value)) el.SetAttribute("impersonationpassword", "");
+                RijndaelManaged aesAlg = null;
+                try
+                {
+                    Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(_key, _salt2);
+                    aesAlg = new RijndaelManaged();
+                    aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+                    aesAlg.IV = key.GetBytes(aesAlg.BlockSize / 8);
+                    ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                    using (MemoryStream msEncrypt = new MemoryStream())
+                    {
+                        using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                            {
+                                swEncrypt.Write(value);
+                            }
+                        }
+                        outStr = Convert.ToBase64String(msEncrypt.ToArray());
+                    }
+                }
+                finally
+                {
+                    if (aesAlg != null) aesAlg.Clear();
+                }
+                el.SetAttribute("impersonationpassword", outStr);
+            }
         }
         public string ImpersonationDomain
         {
