20b1ae0b7938ce97f45b9f65d1ccb2d5de83dc16
nickbro_cp <nick@nbdev.co.uk>
2013-08-31 20:41:33 +0000

    v9.5 release

--- CHS Extranet/HAP.Web/Properties/AssemblyInfo.cs
+++ CHS Extranet/HAP.Web/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("9.5.0807.1400")]
-[assembly: AssemblyFileVersion("9.5.0807.1400")]
+[assembly: AssemblyVersion("9.5.0831.2140")]
+[assembly: AssemblyFileVersion("9.5.0831.2140")]
