b6e34e1be2bb13ff9542b6e9ca088a6aab9e34a8
nickbro_cp <nick@nbdev.co.uk>
2013-12-16 00:10:58 +0000

    Added: New Web Tracker GUI

--- CHS Extranet/HAP.Web/Tracker/WebLogs.aspx.designer.cs
+++ CHS Extranet/HAP.Web/Tracker/WebLogs.aspx.designer.cs
@@ -11,14 +11,5 @@ namespace HAP.Web.Tracker {
     
     
     public partial class WebLogs {
-        
-        /// <summary>
-        /// dates control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater dates;
     }
 }
