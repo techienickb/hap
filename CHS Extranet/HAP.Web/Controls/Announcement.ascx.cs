50a7aed5a3a487187f8fdd04c5b364341b43fd50
nickbro_cp <nick@nbdev.co.uk>
2014-02-22 20:26:27 +0000

    Added: Announcment filtering

--- CHS Extranet/HAP.Web/Controls/Announcement.ascx.cs
+++ CHS Extranet/HAP.Web/Controls/Announcement.ascx.cs
@@ -8,6 +8,7 @@ using System.DirectoryServices.AccountManagement;
 using HAP.Web.Configuration;
 using System.Configuration;
 using System.Xml;
+using System.Text.RegularExpressions;
 
 namespace HAP.Web.Controls
 {
@@ -24,8 +25,39 @@ namespace HAP.Web.Controls
             XmlNode node = doc.SelectSingleNode("/announcement");
             ShowAnnouncement.Checked = bool.Parse(node.Attributes[0].Value);
             htmlannouncement.Text = node.InnerXml.Replace("<![CDATA[ ", "").Replace(" ]]>", "");
-            if (ShowAnnouncement.Checked)
-                AnnouncementText.Text = string.Format("<h1 class=\"Announcement\"><b>Announcement</b><br />{0}</h1>", htmlannouncement.Text);
+            string anan = FilterAnan(htmlannouncement.Text);
+            if (ShowAnnouncement.Checked && !string.IsNullOrWhiteSpace(anan))
+                AnnouncementText.Text = string.Format("<h1 class=\"Announcement\"><b>Announcement</b><br />{0}</h1>", anan);
+        }
+
+        private string FilterAnan(string anan)
+        {
+            string result = anan;
+            Regex r = new Regex("\\[Filter:\\!?.*\\].*\\[/Filter]");
+            foreach (Match m in r.Matches(anan))
+            {
+                Regex r2 = new Regex("\\[Filter:|[\\!\\w\\d\\s\\,\\-\\.]*]");
+                string group = r2.Matches(m.Value)[1].Value;
+                group = group.Remove(group.Length - 1);
+                string[] groups = group.Split(new char[] { ',' });
+                if (isVisible(groups))
+                {
+                    result = result.Replace(m.Value, new Regex("\\[Filter:[\\!\\w\\d\\s\\,\\-\\.]*\\]|\\[\\/Filter\\]").Replace(m.Value, ""));
+                }
+                else result = result.Replace(m.Value, "");
+            }
+            return result.Replace("<p><br></p>", "").Replace("<p></p>", "");
+        }
+
+        private bool isVisible(string[] groups)
+        {
+            bool vis = false;
+            foreach (string s in groups)
+            {
+                bool res = s.StartsWith("!") ? !Page.User.IsInRole(s.Remove(0, 1).Replace("Students", hapConfig.Current.AD.StudentsGroup)) : Page.User.IsInRole(s.Replace("Students", hapConfig.Current.AD.StudentsGroup));
+                if (!vis) vis = res;
+            }
+            return vis;
         }
 
         private bool isShowTo()
