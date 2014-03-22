07697277eafa44a1c88c17af918f92b0e2d4cbc6
nickbro_cp <SND\nickbro_cp>
2014-03-22 17:45:25 +0000

    Moved: Booking System to a Plugin Moved: Timetable to a Plugin

--- CHS Extranet/Core/HAP.Web.Config/Properties/AssemblyInfo.cs
+++ CHS Extranet/Core/HAP.Web.Config/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("10.0.0319.2020")]
-[assembly: AssemblyFileVersion("10.0.0319.2020")]
+[assembly: AssemblyVersion("10.0.0322.1800")]
+[assembly: AssemblyFileVersion("10.0.0322.1800")]
