a2883cc9f4a4cbfe3d206d22b7574450908c0586
nickbro_cp <nick@nbdev.co.uk>
2014-04-01 16:51:20 +0000

    Removed: Excess Code

--- CHS Extranet/HAP.Web/API/JS.cs
+++ CHS Extranet/HAP.Web/API/JS.cs
@@ -131,21 +131,6 @@ namespace HAP.Web.API
         }
 
 
-
-        public static string RemoveWhiteSpaceFromStylesheets(string body)
-        {
-            body = Regex.Replace(body, @"[a-zA-Z]+#", "#");
-            body = Regex.Replace(body, @"[\n\r]+\s*", string.Empty);
-            body = Regex.Replace(body, @"\s+", " ");
-            body = Regex.Replace(body, @"\s?([:,;{}])\s?", "$1");
-            body = body.Replace(";}", "}");
-            body = Regex.Replace(body, @"([\s:]0)(px|pt|%|em)", "$1");
-            // Remove comments from CSS
-            body = Regex.Replace(body, @"/\*[\d\D]*?\*/", string.Empty);
-            return body;
-        }
-
-
         public string[] GetPaths(string Referrer)
         {
             List<string> s = new List<string>();
