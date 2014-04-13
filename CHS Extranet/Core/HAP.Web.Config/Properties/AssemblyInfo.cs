b9d3d4fcc4f622fdb0acea3f2d0e71e0a19e3d3b
nickbro_cp <nick@nbdev.co.uk>
2014-04-13 20:19:29 +0000

    Removed: Static File Handler

--- CHS Extranet/Core/HAP.Web.Config/Properties/AssemblyInfo.cs
+++ CHS Extranet/Core/HAP.Web.Config/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("10.0.0331.1800")]
-[assembly: AssemblyFileVersion("10.0.0331.1800")]
+[assembly: AssemblyVersion("10.0.0413.2100")]
+[assembly: AssemblyFileVersion("10.0.0413.2100")]
