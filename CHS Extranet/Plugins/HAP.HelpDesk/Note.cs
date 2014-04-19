ecd48daf84996a9e252284287c38bd9be63e3a13
nickbro_cp <nick@nbdev.co.uk>
2014-04-19 13:07:05 +0000

    Added: Start of Help Desk SQL Provider

--- CHS Extranet/Plugins/HAP.HelpDesk/Note.cs
+++ CHS Extranet/Plugins/HAP.HelpDesk/Note.cs
@@ -19,7 +19,7 @@ namespace HAP.HelpDesk
 
         public Note(XmlNode node)
         {
-            NoteText = HttpUtility.UrlEncode(node.InnerXml.Replace("<![CDATA[", "").Replace("]]>", "").Replace("\n", "<br />"), System.Text.Encoding.Default); ;
+            NoteText = HttpUtility.UrlEncode(node.InnerXml.Replace("<![CDATA[", "").Replace("]]>", "").Replace("\n", "<br />"), System.Text.Encoding.Default);
             if (node.Attributes["date"] != null && node.Attributes["time"] != null)
                 Date = DateTime.Parse(node.Attributes["date"].Value + " " + node.Attributes["time"].Value).ToString("dd/MM/yy HH:mm");
             else Date = DateTime.Parse(node.Attributes["datetime"].Value).ToString("dd/MM/yy HH:mm");
@@ -30,6 +30,19 @@ namespace HAP.HelpDesk
             }
             catch { DisplayName = "UNKNOWN"; }
             Hide = (node.Attributes["hide"] != null) ? bool.Parse(node.Attributes["hide"].Value) : false;
+        }
+
+        public Note(Data.SQL.Note n)
+        {
+            NoteText = HttpUtility.UrlEncode(n.Content.Replace("<![CDATA[", "").Replace("]]>", "").Replace("\n", "<br />"), System.Text.Encoding.Default);
+            Date = n.DateTime.ToString("dd/MM/yy HH:mm");
+            Username = n.Username;
+            try
+            {
+                DisplayName = ADUtils.FindUserInfos(n.Username)[0].DisplayName;
+            }
+            catch { DisplayName = "UNKNOWN"; }
+            Hide = n.Hide;
         }
     }
 }
